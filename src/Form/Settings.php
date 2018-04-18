44ae7b7928ad04f272f475432d666bc740f3b2ba
dakku <dakku@97634.no-reply.drupal.org>
2018-04-18 15:24:00 +0100

    Add configurable cache options to the settings form.

--- src/Form/Settings.php
+++ src/Form/Settings.php
@@ -69,11 +69,51 @@ class Settings extends ConfigFormBase {
         '#default_value' => $config->get('access_token'),
         '#description' => $this->t('To get your Access Token, @link.',
           [
-            '@link' => Link::fromTextAndUrl('click here', Url::fromUri($this->accessUrl()))->toString()
+            '@link' => Link::fromTextAndUrl('click here', Url::fromUri($this->accessUrl()))->toString(),
           ]),
       ];
     }
 
+
+    $form['caching'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Instagram API Caching'),
+      '#open' => TRUE,
+      '#description' => $this->t('API caching is recommended for all websites.'),
+    ];
+
+    // Identical options to the ones for block caching.
+    // @see \Drupal\Core\Block\BlockBase::buildConfigurationForm()
+    $period = [
+      0,
+      60,
+      180,
+      300,
+      600,
+      900,
+      1800,
+      2700,
+      3600,
+      10800,
+      21600,
+      32400,
+      43200,
+      86400,
+    ];
+
+    $period = array_map([$this->dateFormatter, 'formatInterval'], array_combine($period, $period));
+    $period[0] = '<' . $this->t('no caching') . '>';
+    $form['caching']['api_cache_maximum_age'] = [
+      '#type' => 'select',
+      '#title' => $this->t('API cache maximum age'),
+      '#default_value' => $config->get('api_cache_maximum_age'),
+      '#options' => $period,
+      '#description' => $this->t('The maximum time a API request can be cached by Drupal.'),
+    ];
+
+//    $foo2 = \Drupal::service('instagram_api.tags');
+    $foo2 = \Drupal::service('instagram_api.tags')->tagMediaRecent('drupal');
+    kint($foo2);
     return parent::buildForm($form, $form_state);
   }
 
@@ -85,6 +125,7 @@ class Settings extends ConfigFormBase {
       ->set('client_id', $form_state->getValue('client_id'))
       ->set('client_secret', $form_state->getValue('client_secret'))
       ->set('access_token', $form_state->getValue('access_token'))
+      ->set('api_cache_maximum_age', $form_state->getValue('api_cache_maximum_age'))
       ->save();
 
     parent::submitForm($form, $form_state);
@@ -100,7 +141,7 @@ class Settings extends ConfigFormBase {
     $redirectUrl = Url::fromUri('internal:/instagram_api/callback', ['absolute' => TRUE])->toString();
 
     // TODO Fix this Drupal way.
-    
+
     return $config->get('api_uri') . 'authorize/?client_id=' . $config->get('client_id') . '&response_type=code&redirect_uri=' . $redirectUrl;
   }
 
